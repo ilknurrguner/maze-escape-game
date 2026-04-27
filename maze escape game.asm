964f91a3b7b3f0a5c7b302a8be01464a130ad265
İlknur Güner <ilknurrguner@gmail.com>
2026-04-27 21:56:24 +0300

    Update maze escape game.asm

--- maze escape game.asm
+++ maze escape game.asm
@@ -4,69 +4,68 @@ jmp start
 ;kolay seviye 20x20 labirent tasarimi 
 ;d:duvar, o:bosluk, *:oyuncu, x:cikis
 mazeeasy db 'dddddddddddddddddddd'
-         db '*oodooooooooodododod'
-         db 'ddododddddddododoood'
-         db 'doooodoooodoodooodod'
+         db '*oodoooootooodcdodod'
+         db 'ddododddddddododotod'
+         db 'docoodoooodoodooodod'
          db 'dodddddodddoddddddod'
          db 'dodooododododooooood'
-         db 'dododododooooodddddd'
+         db 'dodcdododooooodddddd'
          db 'dodddododddoddoooood'
-         db 'dooooooodoooooodddod'
+         db 'dooooootdoooooodddod'
          db 'ddddddooooodddododod'
-         db 'dooooddddoodooododod'
-         db 'doddoddoooddododoood'
-         db 'dodooddddoddododdddd'
+         db 'dooooddddotdooododod'
+         db 'doddoddcooddododoood'
+         db 'dodcoddddoddododdddd'
          db 'doddddoooooooooooodd'
-         db 'doooooodddoddddddodd'
-         db 'doddddodododdooododd'
-         db 'dodoodooooodoodddood'
+         db 'doooootdddoddddddodd'
+         db 'dtddddodcdoddoctdodd'
+         db 'dodcodooooodoodddocd'
          db 'doddodddddoooooodddd'
-         db 'doooodoooooddddooood'
+         db 'doooodcooooddddooood'
          db 'ddddddddddddddddddxd'
 
 ;orta seviye 30x20 labirent tasarimi
 mazemedium db 'dddddddddddddddddddddddddddddd'
-           db '*odooodooooooddodddoddodddoddd'
-           db 'dododododdddoodooooooooooododd'
-           db 'dododododdddddddodddddddoddood'
+           db '*odooodooooooddcdddoddodddoddd'
+           db 'dododododdddocdoooootooooododd'
+           db 'dodcdododdddddddodddddddoddood'
            db 'dodddoooooooooddodooooooodddod'
-           db 'dodododddddooddoododdddoodoood'
-           db 'dodododooodododdododooododddod'
-           db 'dodooododddoooooododododooooox'
-           db 'dodddodododdddddddododddoddodd'
-           db 'doooooooooooooododooodoooddood'
-           db 'ddodooddddooodododddddddodoodd'
-           db 'ddoddododoodododooodooododoodd'
-           db 'ddoodoooooodododododddododdodd'
-           db 'ddoddoddddddodoooooooooooooood'
-           db 'ddodooooooodododdddddddddodddd'
-           db 'ddddddddddodddodoodododododood'
-           db 'doodooooooooooododdododoooddod'
+           db 'dodcdodddddotddoododdddoodcood'
+           db 'dodododoocdododdododooododddod'
+           db 'dodooododddoooooodododcdooooox'
+           db 'dodddodtdoddddddddododddoddodd'
+           db 'doooooooooooooododooodcooddotd'
+           db 'ddodooddddooodododddddddodcodd'
+           db 'ddoddtdodoodododooodcotdodoodd'
+           db 'ddocdoooooodododododddododdodd'
+           db 'ddoddoddddddodooooooooooooootd'
+           db 'ddodooooooodtdoddddddddddodddd'
+           db 'ddddddddddodddodocdododododood'
+           db 'doodooooooooooododdododootddod'
            db 'doododddddddodooodoooooooooood'
-           db 'doddoodoodododododododoododddd'
+           db 'doddocdoodododododododoododddd'
            db 'dddddddddddddddddddddddddddddd'
-
 
 ;zor seviye 40x20 labirent tasarimi
 mazehard db 'dddddddddddddddddddddddddddddddddddddddd'
          db '*oooddddddddddddooooddddoooooooooddddddd'
-         db 'dddooooodddooooooddododdodddddddoooodddd'
-         db 'ddoooddodooodddddddoooooodoooooddddodood'
-         db 'ddododdodododoooooodddddddodddodooooddod'
-         db 'ddodododdddoddddddodooooododododdodooood'
-         db 'ddooooooooooooooodododddododododdddddddd'
-         db 'dddddddddododddoddododododododooooooooox'
-         db 'ddoooooooodooododoododododooododdddddodd'
-         db 'dddddddddddodododoodooododddddododdooodd'
-         db 'ddoooooooooodododdoooooooooooooooooodddd'
+         db 'dddooooodddooooooddodcddodddddddoootdddd'
+         db 'ddoooddodooodddddddoooooodoooooddddodcod'
+         db 'ddtdoddodcdodcooooodddddddodddodooooddod'
+         db 'ddcdodtddddoddddddodooooododcdoddodooood'
+         db 'ddooooooooooooooodododddodododtddddddddd'
+         db 'dddddddddtdodddoddtdodcdodododooooooooox'
+         db 'ddcooooooodooododoododododooododdddddodd'
+         db 'dddddddddddodododoodootdodddddodcddtoodd'
+         db 'ddoooooootoodododdoooooooooooooooooodddd'
          db 'ddoddodddddodododdddoddddddddddddddodddd'
-         db 'dooododdoddodododdooooooooooooooooddoood'
+         db 'dooododdcddodododdcoooooooooooootcddoood'
          db 'dodddooooddodododdddddddddoddddddddododd'
-         db 'dodoooddddoododoooooooooooooooddodoododd'
-         db 'dodddddodddddddddddoddddddodooooooooodod'
-         db 'doooooooooooooooddooooooooododddodddodod'
+         db 'dodcooddddoododoooooooooooooooddodoododd'
+         db 'dodddddodddddddddddoddddddodooooooooodcd'
+         db 'dtooooooooooooooddooooooooododddodddodod'
          db 'ddddodddddoddddodoodddddddododododododod'
-         db 'dooooooooooooodododooodoododododododoood'
+         db 'doooooooooooocdcdcdooodoododododododoood'
          db 'dddddddddddddddddddddddddddddddddddddddd'
 
 ;kullaniciya zorluk seviyesi sectiren giris ekrani ve oyun sonunda yazacak mesajlar
@@ -76,6 +75,7 @@ msg3 db 'TIME UP!$'
 msg4 db ' Play again? Y/N: $'
 
 ;oyun icindeki degiskenler
+selectedMaze dw 0
 currentMaze dw 0
 mazeWidth db 0
 mazeHeight db 0
@@ -88,6 +88,16 @@ playerCol db 0
 timeLeft dw 0
 lastSec db 0
 resultRow db 22
+columnCount db 0
+
+;coinlerden kazanilan puani tutar
+coinScore dw 0
+
+;oyun sonunda kalan sure ile coin puanini toplayarak final skoru tutar
+finalScore dw 0
+
+;secilen labirentin kopyalanip oyun sirasinda uzerinde islem yapildigi alan
+workMaze db 800 dup(0)
 
 ;ekrani temizleyip zorluk secim menusunu gosterir 
 start:
@@ -112,7 +122,7 @@ jmp readChoice
 
 ;kolay seviye icin labirent tasarimi ve sureyi ayarlar
 setEasy:
-mov word ptr [currentMaze],offset mazeeasy
+mov word ptr [selectedMaze],offset mazeeasy
 mov byte ptr [mazeWidth],20
 mov byte ptr [mazeHeight],20
 mov word ptr [mazeSize],400
@@ -121,7 +131,7 @@ jmp initGame
 
 ;orta seviye icin labirent tasarimi ve sureyi ayarlar
 setMedium:
-mov word ptr [currentMaze],offset mazemedium
+mov word ptr [selectedMaze],offset mazemedium
 mov byte ptr [mazeWidth],30
 mov byte ptr [mazeHeight],20
 mov word ptr [mazeSize],600
@@ -130,7 +140,7 @@ jmp initGame
 
 ;zor seviye icin labirent tasarimi ve sureyi ayarlar
 setHard:
-mov word ptr [currentMaze],offset mazehard
+mov word ptr [selectedMaze],offset mazehard
 mov byte ptr [mazeWidth],40
 mov byte ptr [mazeHeight],20
 mov word ptr [mazeSize],800
@@ -140,6 +150,12 @@ mov word ptr [timeLeft],90
 initGame:
 mov ax,0003h
 int 10h
+
+;her yeni oyunda labirentin orijinal halini calisma alanina kopyalar
+call CopyMaze
+
+mov word ptr [coinScore],0
+mov word ptr [finalScore],0
 
 mov al,80
 sub al,[mazeWidth]
@@ -161,7 +177,7 @@ mov [lastSec],dh
 
 call FindPlayer
 call DrawMaze
-call DrawTimer
+call DrawStatus
 
 ;sureyi kontrol eder ve klavye tuslarini okur
 gameLoop:
@@ -243,7 +259,7 @@ mov [lastSec],dh
 cmp word ptr [timeLeft],0
 je timeFinish
 dec word ptr [timeLeft]
-call DrawTimer
+call DrawStatus
 cmp word ptr [timeLeft],0
 jne utEnd
 
@@ -251,6 +267,10 @@ timeFinish:
 pop dx
 pop bx
 pop ax
+
+; sure bittiginde final score'u sifirlar
+mov word ptr [finalScore],0
+
 call ShowLose
 call AskReplay
 
@@ -260,8 +280,8 @@ pop bx
 pop ax
 ret
 
-;kalan sureyi ekrana yazdirir
-DrawTimer:
+;kalan sureyi ve mevcut skoru ekrana yazdirir
+DrawStatus:
 push ax
 push bx
 push dx
@@ -287,28 +307,37 @@ mov al,' '
 int 10h
 
 mov ax,[timeLeft]
-xor dx,dx
-mov bx,60
-div bx
+call PrintNumber
 
-add al,'0'
 mov ah,0Eh
+mov al,' '
 int 10h
-
+mov al,' '
+int 10h
+mov al,'S'
+int 10h
+mov al,'C'
+int 10h
+mov al,'O'
+int 10h
+mov al,'R'
+int 10h
+mov al,'E'
+int 10h
 mov al,':'
 int 10h
-
-mov ax,dx
-xor dx,dx
-mov bx,10
-div bx
-
-add al,'0'
-mov ah,0Eh
+mov al,' '
 int 10h
-mov al,dl
-add al,'0'
+
+mov ax,[coinScore]
+call PrintNumber
+
 mov ah,0Eh
+mov al,' '
+int 10h
+mov al,' '
+int 10h
+mov al,' '
 int 10h
 
 pop dx
@@ -319,21 +348,46 @@ ret
 ;sure bittiginde ekrana TIME UP yazdirir
 ShowLose:
 push ax
+push bx
 push dx
 mov ah,02h
 mov bh,0
 mov dh,[msgRow]
 mov dl,[leftMargin]
 int 10h
-mov ah,09h
-lea dx,msg3
-int 21h
+mov dx,offset msg3
+mov bl,0Ch
+call PrintStringColor
+
+mov ah,0Eh
+mov al,' '
+int 10h
+mov al,'S'
+int 10h
+mov al,'C'
+int 10h
+mov al,'O'
+int 10h
+mov al,'R'
+int 10h
+mov al,'E'
+int 10h
+mov al,':'
+int 10h
+mov al,' '
+int 10h
+mov ax,[finalScore]
+call PrintNumber
 pop dx
+pop bx
 pop ax
 ret
 
 ;oyuncu kazandiginda ekrana YOU WIN yazdirir
 ShowWin:
+push ax
+push bx
+push dx
 mov ah,02h
 mov bh,0
 mov dh,[resultRow]
@@ -341,9 +395,32 @@ inc dh
 mov dl,30
 int 10h
 
-mov ah,09h
-lea dx,msg2
-int 21h
+mov dx,offset msg2
+mov bl,0Ah
+call PrintStringColor
+
+mov ah,0Eh
+mov al,' '
+int 10h
+mov al,'S'
+int 10h
+mov al,'C'
+int 10h
+mov al,'O'
+int 10h
+mov al,'R'
+int 10h
+mov al,'E'
+int 10h
+mov al,':'
+int 10h
+mov al,' '
+int 10h
+mov ax,[finalScore]
+call PrintNumber
+pop dx
+pop bx
+pop ax
 ret
 
 ;labirent tasariminda oyuncunun baslayacagi yeri bulur
@@ -374,17 +451,18 @@ ret
 DrawMaze:
 mov si,[currentMaze]
 mov cx,[mazeSize]
-xor bx,bx
+mov byte ptr [columnCount],0
 mov dh,[topMargin]
 mov dl,[leftMargin]
 
 ;labirent verisindeki karaktere gore duvar, bosluk, oyuncu veya cikis cizer
 drawLoop:
-cmp bx,0
+cmp byte ptr [columnCount],0
 jne skipPos
 mov ah,02h
 mov bh,0
 int 10h
+
 skipPos:
 lodsb
 cmp al,'d'
@@ -393,30 +471,67 @@ cmp al,'o'
 je drawEmpty
 cmp al,'*'
 je drawPlayerStart
+cmp al,'x'
+je drawExit
+cmp al,'c'
+je drawCoin
+cmp al,'t'
+je drawTrap
 jmp drawChar 
 
 drawPlayerStart:
 mov al,15
-jmp drawChar
+mov bl,0Dh
+call PrintColor
+jmp afterColorPrint 
+
+drawExit:
+mov al,'X'
+mov bl,0Ah
+call PrintColor
+jmp afterColorPrint
+
+;coin karakterini sari karo olarak ekrana cizer
+drawCoin:
+mov al,4
+mov bl,0Eh
+call PrintColor
+jmp afterColorPrint
+
+;trap karakterini kirmizi yildiz olarak ekrana cizer
+drawTrap:
+mov al,42
+mov bl,0Ch
+call PrintColor
+jmp afterColorPrint
 
 ;d karakterini duvara cevirir
 drawWall:
 mov al,219
-jmp drawChar
+mov bl,09h
+call PrintColor
+jmp afterColorPrint
 
 ;o karakterini bosluga cevirir
 drawEmpty:
 mov al,' '
+mov bl,07h
+call PrintColor
+jmp afterColorPrint
 
 ;hazirlanan karakteri ekrana yazdirir
 drawChar:
 mov ah,0Eh
 int 10h
-inc bx
-cmp bl,[mazeWidth]
+
+afterColorPrint:
+inc byte ptr [columnCount]
+mov al,[columnCount]
+cmp al,[mazeWidth]
 jne drawCont
-xor bx,bx
+mov byte ptr [columnCount],0
 inc dh
+
 drawCont:
 loop drawLoop
 ret
@@ -430,12 +545,53 @@ cmp dl,'d'
 je moveBlocked
 cmp dl,'x'
 je moveToExit
+cmp dl,'c'
+je moveToCoin
+cmp dl,'t'
+je moveToTrap
 
 call EraseOldPlayer
 mov [playerRow],al
 mov [playerCol],bl
 call DrawPlayer
 jmp moveDone
+
+;coin alindiginda puani 5 artirir ve coin hucreyi bosluk yapar
+moveToCoin:
+add word ptr [coinScore],5
+call SetCellEmpty
+call DrawStatus
+call EraseOldPlayer
+mov [playerRow],al
+mov [playerCol],bl
+call DrawPlayer
+jmp moveDone
+
+;trap'e basilinca sureden 5 saniye dusurur ve trap hucreyi bosluk yapar
+moveToTrap:
+cmp word ptr [timeLeft],5
+ja trapSub
+mov word ptr [timeLeft],0
+jmp trapAfter
+trapSub:
+sub word ptr [timeLeft],5
+trapAfter:
+call SetCellEmpty
+call DrawStatus
+call EraseOldPlayer
+mov [playerRow],al
+mov [playerCol],bl
+call DrawPlayer
+cmp word ptr [timeLeft],0
+je trapTimeFinish
+jmp moveDone
+
+trapTimeFinish:
+pop bx
+pop ax
+call CalculateFinalScore
+call ShowLose
+call AskReplay
 
 ;gidilecek yer cikissa oyunu kazanma durumuna gecirir
 moveToExit:
@@ -445,6 +601,7 @@ mov [playerCol],bl
 call DrawPlayer
 pop bx
 pop ax
+call CalculateFinalScore
 call ShowWin
 call AskReplay
 
@@ -463,9 +620,9 @@ push dx
 mov al,[playerRow]
 mov bl,[playerCol]
 call SetCursorByCell
-mov ah,0Eh
 mov al,' '
-int 10h
+mov bl,07h
+call PrintColor
 pop dx
 pop bx
 pop ax
@@ -479,9 +636,9 @@ push dx
 mov al,[playerRow]
 mov bl,[playerCol]
 call SetCursorByCell
-mov ah,0Eh
 mov al,15
-int 10h
+mov bl,0Dh
+call PrintColor
 pop dx
 pop bx
 pop ax
@@ -612,4 +769,108 @@ jmp start
 
 exitGame:
 mov ax,4C00h
-int 21h
+int 21h 
+
+;renkli karakter yazdirir ve cursoru bir sonraki sutuna tasir
+PrintColor:
+push ax
+push bx
+push cx
+
+mov ah,09h
+mov bh,0
+mov cx,1
+int 10h
+
+pop cx
+pop bx
+pop ax
+
+mov ah,0Eh
+int 10h
+ret
+
+;$ isaretine kadar olan yaziyi secilen renkte ekrana basar
+PrintStringColor:
+push ax
+push si
+mov si,dx
+pscLoop:
+lodsb
+cmp al,'$'
+je pscEnd
+call PrintColor
+jmp pscLoop
+pscEnd:
+pop si
+pop ax
+ret
+
+;sayisal degeri ekrana decimal olarak yazdirir
+PrintNumber:
+push ax
+push bx
+push cx
+push dx
+
+cmp ax,0
+jne pnStart
+mov ah,0Eh
+mov al,'0'
+int 10h
+jmp pnEnd
+
+pnStart:
+xor cx,cx
+mov bx,10
+
+pnDiv:
+xor dx,dx
+div bx
+push dx
+inc cx
+cmp ax,0
+jne pnDiv
+
+pnPrint:
+pop dx
+mov ah,0Eh
+mov al,dl
+add al,'0'
+int 10h
+loop pnPrint
+
+pnEnd:
+pop dx
+pop cx
+pop bx
+pop ax
+ret
+
+;oyun sonunda coin puani ile kalan sureyi toplayarak final skoru hesaplar
+CalculateFinalScore:
+push ax
+mov ax,[coinScore]
+add ax,[timeLeft]
+mov [finalScore],ax
+pop ax
+ret
+
+;secilen labirenti orijinal veriden calisma alanina kopyalar
+CopyMaze:
+push ax
+push cx
+push si
+push di
+push ds
+pop es
+mov si,[selectedMaze]
+mov di,offset workMaze
+mov cx,[mazeSize]
+rep movsb
+mov word ptr [currentMaze],offset workMaze
+pop di
+pop si
+pop cx
+pop ax
+ret
